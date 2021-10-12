211fc93e20d6b0037c715c0b606fc3300c826ab9
Nicolas <n_ahn2@u.pacific.edu>
2021-10-12 14:33:43 -0700

    5. Added Command Line GUI and recursion run

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -1,6 +1,7 @@
 import random
 
 import numpy as np
+from pip._internal.utils.misc import enum
 
 from Chromossome import Chromossome
 
@@ -59,11 +60,11 @@ def crossoverUniform(parents, offspringSize):
     #     temp.append(parent)
     for x in range(offspringSize):
         # randomly select parent one and two for crossover
-        print("-- picking crossover: --")
+        # print("-- picking crossover: --")
         rand1 = random.randrange(len(parents))
         rand2 = random.randrange(len(parents))
-        print(rand1)
-        print(rand2)
+        # print(rand1)
+        # print(rand2)
 
 
         # parentOne = parents[random.randrange(len(parents))]
@@ -86,12 +87,12 @@ def uniformUtil(one, two):
         prob = random.uniform(0, 1)
         # 50/50 probability of choosing gene 2
         if random.randint(0, 1) == 1:
-            print("--parente2")
+            # print("--parente2")
             # newGene = one.getGene(x)
             temp.setGene(x, two.getGene(x))
 
     temp.fixRanges()
-    temp.printGenes()
+    # temp.printGenes()
     return temp
 
 
@@ -106,6 +107,17 @@ def mutate(chromossomes, prob):
             if random.random() <= prob:
                 chromossomes[x].mutateGene(gene)
 
+
+# print the Max Min and Median fitness scores every 10 runs
+def printMaxMinAvg(population, runs):
+    if runs % 10 == 0:
+        #finding max:
+        print("\n--- Max, Min and Median Fitness Scores after " + str(runs) + " runs ---")
+        print(" Max fitness score: " + str(population[0].getFitnessScore()))
+        print(" Min fitness score: " + str(population[-1].getFitnessScore()))
+
+        center = np.take(population, len(population)/2)
+        print(" Median fitness score: " + str(center.getFitnessScore()))
 
 class GeneticAlgorithm:
     # chromossomes = []
@@ -127,27 +139,6 @@ class GeneticAlgorithm:
         self.chromossomes = []
         self.numOfSelectedChromossomes = 0
         # self.runTest()
-
-    def run(self):
-        # asking for user for a data file
-        while self.file is None:
-            fileName = input("Name of the file to be used (without extension): ")
-            self.file = readFile(fileName)
-        # file = readFile("genAlgData1")
-        print(self.file)
-
-        # asking for number of chromossomes in each generation
-        while True:
-            chromNum = input("How many Chromossomes should each Generation have?: ")
-            try:
-                result = validate_numeric(chromNum, int)
-            except ValueError:
-                print("You did not input a number, try again.")
-                continue
-            else:
-                break
-
-        return 0
 
     def runTest(self):
         self.file = readFile("genAlgData1")
@@ -178,5 +169,111 @@ class GeneticAlgorithm:
 
         print("\n-- MUTATION --")
         mutate(nextGen, 0.1)
+
+    # should be controllable:
+    #   - fineName:String = name of the file
+    #   - popSize:int = number of Chromossomes in each Generation
+    #   - runs:int = number of Generations that algorithms will run before terminating
+    #   - selectionType:String = which selection algorithm to use (elitist/tournament)
+    #   - selectionProb:float  = What percentage of the next generation should be formed using selection
+    #       - ex: 40% of 150 = 60 chromos cloned, 90 generated from crossover
+    #       - mutation is applied randomly
+    #   - crossoverType:String = Which crossover algorithm to use (uniform or kpoint).
+    #   - mutationProb:float = The initial mutation rate and how much to change the mutation rate each generation
+    #       – it should be possible to have a fixed mutation rate or to have a mutation rate
+    #           that gradually decreases (without reaching zero).
+    def run(self):
+        # 1. asking for user for a data file: String
+        while self.file is None:
+            fileName = input("Name of the file to be used (without extension): ")
+            self.file = readFile(fileName)
+
+
+        # 2. asking for number of chromossomes in each generation (population):int
+        while True:
+            popSize = input("How many Chromossomes should each Generation have?: ")
+            try:
+                result = validate_numeric(popSize, int)
+            except ValueError:
+                print("You did not input a number, try again.")
+                continue
+            else:
+                break
+
+        self.popSize = int(popSize)
+        #generating initial population
+        initialPop = generateChromossomes(self.popSize, self.file)
+
+        # asking for number of selected chromossomes:int
+        self.numOfSelectedChromossomes = int(input("How many Chromossomes should be selected to the next generation?"))
+
+        # Ask for how many runs before the program should terminate
+        self.totalRuns = int(input("how many runs before the program should terminate?"))
+
+        # asking selection type and prob:String
+        self.selectionType = int(input("What type of selection do you want? Elitist [1] or Tournament [2]"))
+
+        # self.selectionProb = input("What should be the ")  this is numOfSelectedChromossomes
+
+        #Asking for crossover Type
+        self.crossoverType = int(input("What should be the crossover type be? Uniform [1]"))
+
+
+        #Mutation Rate
+        mutationProb = 0.1
+
+        #Ask if there should be decreasae in mutation rate
+        foo = input("Would you like a flat mutation rate [1] or a gradual decrease [2]?")
+        if foo == 1:
+            self.mutationVariation = False
+        else:
+            self.mutationVariation = True
+
+        self.runAlgoRecursive(0, initialPop, mutationProb)
+
+    # input:
+    #   - run: int = run number
+    #   - population : List<Chromossomes> = list of current population of chromossomes
+    #   - mutationProb : float = probability for a gene mutation
+    #   - gradualDecrease : Boolean = whether or not the mutation rate is flat.
+    # Output:
+    #   - print max, min and avg fitness scores after each generation
+    #   - At final generation: print Chromossome with highest fitness score and its score
+    def runAlgoRecursive(self, run, population, mutationProb):
+        # print("----RECURSIVE: RUNS = " + str(runs))
+        if run == self.totalRuns:
+            print("\n--- Final Generation ---\n")
+            print(" Highest fit chromossome = ")
+            population[0].printGenes()
+            print(" Fitness score = " + str(population[0].getFitnessScore()))
+            return
+
+        ## Step 1: Selection
+        if self.selectionType == 1:
+            # list of Chromossomes selected by Elitist selection
+            parents = elitistSelection(population, self.numOfSelectedChromossomes)
+
+        # number of offsprings to be created
+        offspringSize = self.popSize - self.numOfSelectedChromossomes
+
+        ## Step 2: Crossover
+        #list containing the parent Chromossomes and offsprings
+        if self.crossoverType == 1:
+            nextGen = crossoverUniform(parents, offspringSize)
+
+        ## Step 3: Mutation
+        mutate(nextGen, mutationProb)
+        if self.mutationVariation is True:
+            mutationProb = mutationProb - (mutationProb*0.10)
+
+        mutate(nextGen, mutationProb)
+
+        printMaxMinAvg(population, run)
+
+        self.runAlgoRecursive(run+1, nextGen, mutationProb)
+
+
+
+
 
 
